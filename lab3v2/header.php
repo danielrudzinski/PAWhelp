85645fee1f940e62fcb9a2eb3e54dce9152ba974
danielrudzinski <115213242+danielrudzinski@users.noreply.github.com>
2025-01-06 22:22:15 +0100

    dasdsa
    dassda

---
+++ lab3v2/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Witam na stronie o oscarach</title>
+</head>
+<body>
+    <header>
+        <h1>Witam na stronie o oscarach</h1>
+        <nav>
+            <a href="index.php">Home</a> |
+            <a href="about.php">About</a> |
+            <a href="contact.php">Contact</a>
+        </nav>
+        <hr>
+    </header>
